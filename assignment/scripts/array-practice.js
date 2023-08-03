01c11fbaa5cbd11891203f361311d5d9416701f3
jcolago <jcolago2005@gmail.com>
2023-08-02 21:08:02 -0500

    convert favorite foods to string

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -99,7 +99,7 @@ console.log(favfoodsArray);
 // 4.g (STRETCH) TODO: Convert your array to a string
 //     putting the word "and" between each item.
 //     eg "tacos and pizza and pasta". Log the string.
-
+console.log(favfoodsArray.join(" and "))
 // 4.h (STRETCH) TODO: Make a new array that combines 
 //     the favorite foods array with the animals array.
 //     Then log the new array.
